3eb1a27d8ef720de3660b9d4d4693c3940856a3e
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2020-05-08 00:19:58 -0400

    Add comment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { useState, useEffect } from 'react';
 import RssParser from 'rss-parser';
 
+/**
+ * This function can be used to retrieve an RSS Feed within React function components
+ * {@link https://reactjs.org/docs/hooks-intro.html as a hook}.
+ * 
+ * @param {string} url The URL of the rss/atom feed.
+ * @param {RssParser.ParserOptions} opts Options for the underlying RssParser
+ * @see {@link https://www.npmjs.com/package/rss-parser rss-parser}
+ */
 export const useRss = (url: string, opts?: RssParser.ParserOptions) => {
   const rss = new RssParser(opts);
   const [rssOutput, setRssOutput] = useState<RssParser.Output | null>(null);
